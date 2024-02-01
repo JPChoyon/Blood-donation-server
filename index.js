e442536de01e70b58a0e8a2b05eaae21f5758173
nazmulhossen1 <nazmulhossen.main@gmail.com>
2024-02-01 23:11:45 +0600

    make api for user data porst on backend

--- index.js
+++ index.js
@@ -2,17 +2,15 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-require("dotenv").config()
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-// middleware 
+// middleware
 app.use(cors());
 app.use(express.json());
 
-// mongodb connection 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.fycfdwn.mongodb.net/?retryWrites=true&w=majority`
-
-
+// mongodb connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.fycfdwn.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -20,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -29,44 +27,51 @@ async function run() {
     const postCollection = client.db("bloodDonation").collection("posts");
     const requestCollection = client.db("bloodDonation").collection("request");
 
-
-    // get all user from database 
-    app.get('/users', async (req, res) => {
-      const result = await userCollection.find().toArray()
-      res.send(result)
-    })
-
-    app.post('/users', async (req, res) => {
-      const users = req.body;
-      const result = await userCollection.insertOne(users);
-      res.send(result);
-    });
-    // get all post from database 
-    app.get('/posts', async (req, res) => {
-      const result = await postCollection.find().toArray()
-      res.send(result)
-    })
-
-    app.post('/posts', async (req, res) => {
-      const users = req.body;
-      const result = await postCollection.insertOne(users);
+    /*==================== user related api ============================*/
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const exitingUser = await userCollection.findOne(query);
+      if (exitingUser) {
+        return res.send({ message: "user already exists" });
+      }
+      const result = await userCollection.insertOne(user);
       res.send(result);
     });
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // get all user from database
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    /*==================== user related api ============================*/
+    app.post("/posts", async (req, res) => {
+      const users = req.body;
+      const result = await postCollection.insertOne(posts);
+      res.send(result);
+    });
+
+    // get all post from database
+    app.get("/posts", async (req, res) => {
+      const result = await postCollection.find().toArray();
+      res.send(result);
+    });
+
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-
     // await client.close();
   }
 }
-run().catch(console.dir);;
-
+run().catch(console.dir);
 
 // server runnig
-app.get('/', (req, res) => {
-  res.send('user mangement is on')
-})
+app.get("/", (req, res) => {
+  res.send("Blood Donation servier is running");
+});
 app.listen(port, () => {
-  console.log('server runnig at port', port);
-})
+  console.log("server runnig at port", port);
+});
